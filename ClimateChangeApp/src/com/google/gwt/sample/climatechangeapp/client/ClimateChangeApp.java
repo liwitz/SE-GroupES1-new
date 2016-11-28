98b8ee0246065fd2b08cce33db0cce8fd557f16a
ccocci <carla.coccia@hispeed.ch>
2016-11-28 17:43:23 +0100

    WorldmapMenu

--- ClimateChangeApp/src/com/google/gwt/sample/climatechangeapp/client/ClimateChangeApp.java
+++ ClimateChangeApp/src/com/google/gwt/sample/climatechangeapp/client/ClimateChangeApp.java
@@ -35,29 +35,32 @@ import com.google.gwt.user.client.ui.DockLayoutPanel;
  *
  */
 
-public class ClimateChangeApp extends DockLayoutPanel implements EntryPoint {
+public class ClimateChangeApp extends TabLayoutPanel implements EntryPoint {
 	
 	private WorldMapView worldMapView = new WorldMapView();
 	private TableView tableView = new TableView();
+	private WorldMapMenu worldMapMenu = new WorldMapMenu();
+	private DockLayoutPanel dockLayoutPanel = new DockLayoutPanel(Style.Unit.EM);
 	
 	//private Button exportButton= new Button("Export data");
 	//private Data data;
 	
 	public ClimateChangeApp(){
 		
-		//Creates DockLayoutPanel
-		super(Style.Unit.EM);
-				
+		//Creates TabLayoutPanel
+		super(9, Style.Unit.EM);
+		worldMapMenu.setHeight("100%");		
 		//Creates TabLayoutPanel with two tabs
-		TabLayoutPanel menu = new TabLayoutPanel(9, Style.Unit.EM);
 		
-		menu.add(worldMapView, "World Map");
-		menu.add(tableView, "Data Table");
-		menu.addStyleName("tabs");
-		menu.selectTab(0);
-
+		
+		add(dockLayoutPanel, "World Map");
+		add(tableView, "Data Table");
+		addStyleName("tabs");
+		selectTab(0);
+		
+		dockLayoutPanel.addEast(worldMapMenu,15);
+		dockLayoutPanel.add(worldMapView);
 		//Add tabPanel to north
-		addNorth(menu,40);
 		//addSouth(exportButton, 10);
 	}
 	
